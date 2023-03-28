0e4de69477c2f5893ebd4e39c946523e23f8799a
safar0101 <safar@0101>
2023-03-28 15:17:23 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Gallery from './pages/gallery/Gallery';
+
+function App() {
+  return (
+    
+    <Gallery/>
+  );
+}
+
+export default App;
